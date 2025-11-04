7ffc098b77a21f90529f52471dc6c8732679d132
James Ha <james.ha86@gmail.com>
2025-11-04 18:39:44 -0500

    updates on the configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
 import { AuthModule } from './auth/auth.module';
-import { dataSourceOptions } from './data-source';
+import { TypeOrmConfigService } from './data-source';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     // Use the new data-source options
-    TypeOrmModule.forRoot(dataSourceOptions),
+    // TypeOrmModule.forRoot(dataSourceOptions),
+    ConfigModule.forRoot({
+      // Optional: Specify a custom path for your .env file
+      // envFilePath: '.development.env',
+      // Optional: Specify multiple paths, the first one found takes precedence
+      // envFilePath: ['.env.development.local', '.env.development'],
+      // Optional: Make ConfigModule global so ConfigService can be injected anywhere
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
     AuthModule,
     ArticleModule,
     UserModule,
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,41 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
+// import { DataSource, DataSourceOptions } from 'typeorm';
 
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'password',
-  database: 'wuv-web',
-  synchronize: true, // Be cautious with this in production
-  entities: ['dist/**/*.entity.js'],
-  migrations: [`${__dirname}/db/migrations/**/*.{j,t}s`],
-};
+// export const dataSourceOptions: DataSourceOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'password',
+//   database: 'wuv-web',
+//   synchronize: true, // Be cautious with this in production
+//   entities: ['dist/**/*.entity.js'],
+//   migrations: [`${__dirname}/db/migrations/**/*.{j,t}s`],
+// };
 
-const dataSource = new DataSource(dataSourceOptions);
+// const dataSource = new DataSource(dataSourceOptions);
 
-export default dataSource;
+// export default dataSource;
+
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_NAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_DATABASE'),
+      ssl: true,
+      entities: ['dist/**/*.entity.js'],
+      migrations: [`${__dirname}/db/migrations/**/*.{j,t}s`],
+      synchronize: true,
+    };
+  }
+}
